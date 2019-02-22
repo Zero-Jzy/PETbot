f00e5cd896e941b7295c7306d69c65a070523c11
ngoccp1234 <ngoclath1807071@fpt.edu.vn>
2019-02-22 16:16:29 +0700

    thêm trang about-us và css

--- controllers/other.controller.js
+++ controllers/other.controller.js
@@ -1,3 +1,7 @@
-exports.index = function (req, res) {
+exports.contact = function (req, res) {
     res.render('client/contact.ejs')
+};
+
+exports.about_us = function (req, res) {
+    res.render('client/about-us.ejs')
 };
