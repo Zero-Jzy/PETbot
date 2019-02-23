b905fab26e13dd1d246dc6d185a00d58eda97213
Anh <anh@fpt.edu.vn>
2019-02-24 02:34:12 +0700

    them thong tin phan danh muc trang categories

--- routers/other.route.js
+++ routers/other.route.js
@@ -4,5 +4,6 @@ var controller = require('../controllers/other.controller');
 var router = express.Router();
 
 router.get('/contact', controller.index);
+router.get('/categories', controller.index1);
 
 module.exports = router;
